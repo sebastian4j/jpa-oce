f993f9650b0c27483a7c358b3ccf3d5d6b483550
Sebastian Avila A <sebastian4j@gmail.com>
2018-09-30 20:41:59 -0300

    no necesario

--- demo-fx/lanzar.sh
+++
@@ -1,1 +0,0 @@
-/usr/lib64/jvm/java-1.8.0-openjdk/bin/java -cp target/demo-fx-0.0.1-SNAPSHOT.jar com.sebastian.demo.fx.FxApp
--- demo-fx/src/main/java/com/sebastian/demo/fx/FxAppController.java
+++
@@ -1,7 +0,0 @@
-package com.sebastian.demo.fx;
-
-import javafx.fxml.FXML;
-
-public class FxAppController {
-
-}
